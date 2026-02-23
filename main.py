838bcd4c0ae0d4804417720045f8e3b7ff9ab232
Siddharth <siddharthbaasri@gmail.com>
2026-02-22 17:05:47 -0800

    Created overall structure for dataset generation

--- main.py
+++ main.py
@@ -1,19 +1,61 @@
 import pandas as pd
 from datasets import load_dataset
 from llm import LLM
+from typing import List, Tuple
 import os
 
 #Hyperparams to tune
 BATCH_SIZE = 9000
 #Different model used for now
 MODEL_NAME = "openai/gpt-oss-20b"
+NUM_ATTEMPTS = 4
+DIFFICULTIES = {1: "Easy", 2: "Medium", 3: "Hard"}
+ENTAILMENT_THRESH = 0.5
 
-def generate_hallucinated_answer(question: str, knowledge: str, ground_truth: str, llm_model: LLM) -> str:
-    prompt = f"#Question#: {question}\n\n#Knowledge#: {knowledge}\n\n#Ground truth answer#: {ground_truth}\n\n#Hallucinated Answer#:"
-    
-    #For now, we only get the response and print it. Code for quality control is required.
-    response = llm_model.get_hallucinated_response(prompt)
-    return response
+#Can update the inputs and outputs of these methods
+def evaluate_response_quality(hallu_response: str, ground_truth: str) -> List[bool]:
+    return [True]
+
+def optimize_with_textgrad():
+    pass
+
+def find_difficulty(results):
+    pass
+
+def get_entailment(hallu_response: str, ground_truth: str):
+    return 0
+
+def get_min_similarity(hallucinations):
+    pass
+
+def generate_hallucinated_answer(question: str, knowledge: str, ground_truth: str, llm_model: LLM) -> Tuple[str, int]:
+    hallucinations = []
+    for attempt in range(NUM_ATTEMPTS):
+        print(f"Attempt {attempt + 1}")
+        prompt = f"#Question#: {question}\n\n#Knowledge#: {knowledge}\n\n#Ground truth answer#: {ground_truth}\n\n#Hallucinated Answer#:"
+        
+        #For now, we only get the response and print it. Code for quality control is required.
+        hallu_response = llm_model.get_hallucinated_response(prompt)
+
+        results = evaluate_response_quality(hallu_response, ground_truth)
+        #Must calculate entailment
+        entailment_score = get_entailment(hallu_response, ground_truth)
+        if any(results) and entailment_score < ENTAILMENT_THRESH:
+            difficulty = find_difficulty(results)
+            return hallu_response, difficulty
+        
+        optimized_hallu_response = optimize_with_textgrad()
+        
+        results = evaluate_response_quality(optimized_hallu_response, ground_truth)
+        entailment_score = get_entailment(hallu_response, ground_truth)
+        if any(results) and entailment_score < ENTAILMENT_THRESH:
+            difficulty = find_difficulty(results)
+            return optimized_hallu_response, difficulty
+        
+        hallucinations.append(optimized_hallu_response)
+
+    min_hallu = get_min_similarity(hallucinations)
+    return min_hallu, 1
 
 def main():
     #Load the data locally, or if it isn't there, from hugging face
